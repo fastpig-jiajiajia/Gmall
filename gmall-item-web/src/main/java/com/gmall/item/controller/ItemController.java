7570d0063869e9f2fe1c3f22d457856aea0fe4fc
Hello-World-X <84868514@qq.com>
2019-11-08 11:34:52 +0800

    feature2 第一次提交 11-8

--- gmall-item-web/src/main/java/com/gmall/item/controller/ItemController.java
+++ gmall-item-web/src/main/java/com/gmall/item/controller/ItemController.java
@@ -1,14 +1,31 @@
 package com.gmall.item.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ItemController {
 
     @RequestMapping("index")
-    public String index(){
+    public String index(ModelMap modelMap){
+
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i <5 ; i++) {
+            list.add("循环数据"+i);
+        }
+
+        modelMap.put("list",list);
+        modelMap.put("hello","hello thymeleaf !!");
+
+        modelMap.put("check","0");
+
+
         return "index";
     }
 

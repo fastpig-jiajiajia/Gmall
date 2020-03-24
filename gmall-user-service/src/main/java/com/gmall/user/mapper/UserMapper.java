82ca54345358233ef75365468ff8b79d840720fc
Hello-World-X <84868514@qq.com>
2020-03-24 23:43:54 +0800

    spring security 权限添加

--- gmall-user-service/src/main/java/com/gmall/user/mapper/UserMapper.java
+++ gmall-user-service/src/main/java/com/gmall/user/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.gmall.user.mapper;
 
 import com.gmall.entity.UmsMember;
+import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
 
 import java.util.List;
@@ -8,5 +9,7 @@ import java.util.List;
 public interface UserMapper extends Mapper<UmsMember> {
 
     List<UmsMember> selectAllUser();
+
+    UmsMember selectUserOfRolesMap(@Param("username") String username);
 
 }
--- gmall-user-service/src/main/java/com/gmall/user/service/impl/UserServiceImpl.java
+++ gmall-user-service/src/main/java/com/gmall/user/service/impl/UserServiceImpl.java
@@ -5,17 +5,21 @@ import com.alibaba.dubbo.config.annotation.Service;
 import com.alibaba.fastjson.JSON;
 import com.gmall.entity.UmsMember;
 import com.gmall.entity.UmsMemberReceiveAddress;
+import com.gmall.entity.UmsRole;
 import com.gmall.service.UserService;
 import com.gmall.user.mapper.UmsMemberReceiveAddressMapper;
 import com.gmall.user.mapper.UserMapper;
 import com.gmall.util.RedisUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.util.CollectionUtils;
 import redis.clients.jedis.Jedis;
 
 import java.util.ArrayList;
@@ -153,13 +157,11 @@ public class UserServiceImpl implements UserService, UserDetailsService {
         // 这里可以捕获异常，使用异常映射，抛出指定的提示信息
         // 用户校验的操作
         // 假设密码是数据库查询的 123
-        UmsMember umsMember = new UmsMember();
-        umsMember.setUsername(username);
-        UmsMember umsMember1 = loginFromDb(umsMember);
+        UmsMember umsMember = userMapper.selectUserOfRolesMap(username);
 
         String password = "$2a$10$XcigeMfToGQ2bqRToFtUi.sG1V.HhrJV6RBjji1yncXReSNNIPl1K";
         // 假设角色是数据库查询的
-        List<String> roleList = userRoleMapper.selectByUserName(username) ;
+        List<UmsRole> roleList = umsMember.getUmsRoleList();
         List<GrantedAuthority> grantedAuthorityList = new ArrayList<>() ;
         /*
          * Spring Boot 2.0 版本踩坑
@@ -167,11 +169,12 @@ public class UserServiceImpl implements UserService, UserDetailsService {
          * 如果不加前缀一般就会出现403错误
          * 在给用户赋权限时,数据库存储必须是完整的权限标识ROLE_LEVEL1
          */
-        if (roleList != null && roleList.size()>0){
-            for (String role : roleList){
-                grantedAuthorityList.add(new SimpleGrantedAuthority(role)) ;
+        if (CollectionUtils.isEmpty(roleList)){
+            for (UmsRole role : roleList){
+                grantedAuthorityList.add(new SimpleGrantedAuthority(role.getName())) ;
             }
         }
-        return new User(username,password,grantedAuthorityList);
+
+        return new User(username, password, grantedAuthorityList);
     }
 }
